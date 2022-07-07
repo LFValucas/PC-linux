212fa25d09e8c15de9cb7c9b3787da9637d950ca
Fernando <fernando.valucas@hotmail.com>
2022-07-06 21:53:09 -0300

    Criando módulos no angular

--- testes-com-angular/teste01/src/app/app.module.ts
+++ testes-com-angular/teste01/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule
